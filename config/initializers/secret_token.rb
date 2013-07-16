96e7e9b7e141364903cb3e816d8a62dd89d3d588
Mike <jarrellmike@gmail.com>
2013-07-17 09:12:21 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paintr::Application.config.secret_token = '6afe35549d78826e5fadd0f771183312bbfddc98c972eb1e80d39420d06d87db46669c93cafe37aecded359f450a94ca74f7c61776ee070eb7105990ede86dc7'
